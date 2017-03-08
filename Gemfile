d8b101234915a0b735b8a1057e62f99281916f40
Saimon GOKYU <sai590817@gmail.com>
2017-03-08 13:11:34 +0000

    DIVE09

--- Gemfile
+++ Gemfile
@@ -60,3 +60,5 @@ gem 'jquery-turbolinks'
 gem 'rails_12factor', group: :production
 
 gem 'devise'
+
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: "test_#{n}"
+               )
+end
+
+n = 1
+while n <= 100
+  Blog.create(
+    title: "あああ",
+    content: "いいい",
+    user_id: n
+  )
+  n = n + 1
+end
