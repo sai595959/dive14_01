57a8d1f81ab45576a8dd8e910db8e4f0a008d3c4
John Doe <johndoe@example.com>
2017-03-24 23:49:18 +0000

    DIVE11_kadai

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -19,32 +19,33 @@ class BlogsController < ApplicationController
     @blog.user_id = current_user.id
     if @blog.save
       redirect_to blogs_path, notice: "ブログを作成しました！"
+      NoticeMailer.sendmail_blog(@blog).deliver
     else
       render 'new'
     end
   end
-  
+
   def edit
     set_blog
   end
-  
+
   def update
     set_blog
     @blog.update(blogs_params)
     redirect_to blogs_path, notice: "プログを編集しました！"
   end
-  
+
   def destroy
     set_blog
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
   end
-  
+
   def confirm
     @blog = Blog.new(blogs_params)
     render :new if @blog.invalid?
   end
-  
+
   private
     def blogs_params
       params.require(:blog).permit(:title, :content)
--- config/routes.rb
+++ config/routes.rb
@@ -7,19 +7,21 @@ Rails.application.routes.draw do
             post :confirm
         end
     end
-    
+
     resources :contacts, only:[:new, :create] do
         collection do
             post :confirm
         end
     end
-    
+
     root 'top#index'
-    
-    
-    
+
+    if Rails.env.development?
+      mount LetterOpenerWeb::Engine, at: "/letter_opener"
+    end
+
     #get 'blogs' => 'blogs#index'
-  
-  
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
